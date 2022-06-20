6ec1af874335e2130fe23bb2e4be81328416bc27
Agustín Martinez4444 <agustinmcuello@gmail.com>
2022-06-20 11:32:36 -0300

    Nuevo cambio

--- src/App.js
+++ src/App.js
@@ -9,13 +9,19 @@ function App() {
   const [lista, setLista] = useState([])
   const agregarElemento=(elementoAAgregar)=>{
     setLista([...lista,elementoAAgregar])
-  }
+  };
+ const borrarTarea=(elementoAQuitar)=>{
+  const listaFiltrada = lista.filter((element)=> element.description !== elementoAQuitar)
+  setLista(listaFiltrada)
+ }
+
+
   return (
     <div className="App">
       <main>
         <h1>Lista de tareas!</h1>
         <Form agregarElemento={agregarElemento}/>
-        <Lista tareas={lista}/>
+        <Lista tareas={lista} borrarTarea={borrarTarea}/>
       </main>
     </div>
   );
--- src/componentes/Lista/Lista.jsx
+++ src/componentes/Lista/Lista.jsx
@@ -1,12 +1,20 @@
 import React from "react";
 import "./lista.css";
 
-function Lista({ tareas }) {
+function Lista({ tareas, borrarTarea }) {
+  const quitarTarea = (ev) => {
+    borrarTarea(ev.target.firstChild.data);
+  };
+  const mostrarTarea = tareas.map((tarea) => (
+    <li onClick={quitarTarea} key={tarea.description}>
+      {tarea.description}
+    </li>
+  ));
   return (
     <>
       <h3>Tareas</h3>
       <p id="mensaje-lista-vacia">Parece que no hay nada por aquí!</p>
-      <ul id="lista-tareas">{tareas.toString()}</ul>
+      <ul id="lista-tareas">{mostrarTarea}</ul>
       <p className="tips">Tip: Pueden borrar tareas clickeando en ellas</p>
     </>
   );
